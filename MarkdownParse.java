9127e15219afc2eb64302fdfcf5d838fa2624ab4
mdsflyboy <mdsflyboy@gmail.com>
2022-04-15 16:49:28 -0700

    Fixing bug in MarkdownParse.java that breaks it when file doesn't end with ")"

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,8 +11,14 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while (currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            // If indexOf can't find an opening bracket
+            if (openBracket == -1) {
+                // Then we are done!
+                // Let's end this loop
+                break;
+            }
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
@@ -23,11 +29,10 @@ public class MarkdownParse {
         return toReturn;
     }
 
-
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
     }
 }
