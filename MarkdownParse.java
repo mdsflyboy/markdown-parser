d06331a09cae13827b799bacc0062753ac95e84f
mdsflyboy <mdsflyboy@gmail.com>
2022-04-15 16:44:02 -0700

    Revert "Fixing MarkdownParse.java" Accidently committed solution
    This reverts commit dcb51696b38129a504035fe88a88404468671f4a.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -6,32 +6,28 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while (currentIndex < markdown.length()) {
+        while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            if (openBracket == -1) {
-                // Cannot find the next bracket!
-                // Let's end this loop
-                break;
-            }
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-
             currentIndex = closeParen + 1;
         }
 
         return toReturn;
     }
 
+
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-        System.out.println(links);
+	    System.out.println(links);
     }
 }
--- breaking-file.md
+++
@@ -1,7 +0,0 @@
-# Title
-
-[link1](https://something.com)
-[link2](some-thing.html)
-
-Since this file does not end with a link, the loop in the getLinks
-never ends!
