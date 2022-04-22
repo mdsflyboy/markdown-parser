80103766c7e2ae1ebf9f17e7a431d963095854e7
mdsflyboy <mdsflyboy@gmail.com>
2022-04-22 16:42:37 -0700

    Adding Test to MarkdownPaseTestfor test-file.md
    clear && javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" Mar
    kdownParseTest.java && java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1
    .3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,13 @@
 
 // Imports tools from junit
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 // Class definition
@@ -13,4 +20,14 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void getLinksTest() throws IOException {
+        Path file = Path.of("test-file.md");
+        String content = Files.readString(file);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expectedLinks = List.of(
+                "https://something.com",
+                "some-thing.html");
+        assertEquals(expectedLinks.toArray(), links.toArray());
+    }
 }
