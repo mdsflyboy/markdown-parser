da50b381105f9b8eb0ede38fedaca44a5656e2db
mdsflyboy <mdsflyboy@gmail.com>
2022-05-06 16:56:59 -0700

    Removing test change comment

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 
 public class MarkdownParse {
 
-    // Test change
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
