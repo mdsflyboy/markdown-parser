a8d86d2cbc1c625aed657401291086b249860fcd
mdsflyboy <mdsflyboy@gmail.com>
2022-05-22 13:53:31 -0700

    Adding snippet tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,17 +12,37 @@ import org.junit.Test;
 
 // Class definition
 public class MarkdownParseTest {
-    // Defines following function as a Test
+
     @Test
-    // Method definition
-    public void addition() {
-        // Checks that 1+1 = 2
-        assertEquals(2, 1 + 1);
+    public void snippet1Test() throws IOException {
+        ArrayList<String> links = getLinksTester("snippet-1.md");
+        List<String> expectedLinks = List.of(
+            "`google.com",
+            "google.com",
+            "ucsd.edu"
+        );
+        System.out.println(links.toArray());
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
     }
 
     @Test
-    public void thisTestNoLongerFails() {
-        assertEquals(-1.0 / 12, 11.0 / 12 - 1.0, 0.1);
+    public void snippet2Test() throws IOException {
+        ArrayList<String> links = getLinksTester("snippet-2.md");
+        List<String> expectedLinks = List.of(
+            "a.com",
+            "a.com(())",
+            "example.com"
+        );
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void snippet3Test() throws IOException {
+        ArrayList<String> links = getLinksTester("snippet-3.md");
+        List<String> expectedLinks = List.of(
+            "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"
+        );
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
     }
 
     private ArrayList<String> getLinksTester(String fileName) throws IOException {
@@ -30,76 +50,5 @@ public class MarkdownParseTest {
         String content = Files.readString(file);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         return links;
-    }
-
-    @Test
-    public void testFileTest() throws IOException {
-        ArrayList<String> links = getLinksTester("test-file.md");
-        List<String> expectedLinks = List.of(
-                "https://something.com",
-                "some-thing.html");
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-    }
-
-    @Test
-    public void breakingFileTest() throws IOException {
-        ArrayList<String> links = getLinksTester("breaking-file.md");
-        List<String> expectedLinks = List.of(
-                "https://something.com",
-                "some-thing.html");
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-    }
-
-    @Test
-    public void imageBugTester() throws IOException {
-        ArrayList<String> links = getLinksTester("image-bug.md");
-        List<String> expectedLinks = List.of(
-                "https://something.com",
-                "some-thing.html");
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-    }
-
-    @Test
-    public void spacedLinkTester() throws IOException {
-        ArrayList<String> links = getLinksTester("spaced-link.md");
-        List<String> expectedLinks = List.of();
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-    }
-
-    @Test
-    public void noLinksTester() throws IOException {
-        ArrayList<String> links = getLinksTester("no-links.md");
-        List<String> expectedLinks = List.of();
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-    }
-
-    @Test
-    public void spacedLinkTester2() throws IOException {
-        ArrayList<String> links = getLinksTester("spaced-links-with-other-links.md");
-        List<String> expectedLinks = List.of(
-                "google.com");
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-    }
-
-    @Test
-    public void newFailedTest() throws IOException {
-        ArrayList<String> links = getLinksTester("nestedParenthesisFailure.md");
-        List<String> expectedLinks = List.of();
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-    }
-
-    @Test
-    public void group2Break() throws IOException {
-        ArrayList<String> links = getLinksTester("group2-break.md");
-        List<String> expectedLinks = List.of();
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
-    }
-
-    @Test
-    public void test3() throws IOException {
-        ArrayList<String> links = getLinksTester("one-space-file.md");
-        List<String> expectedLinks = List.of();
-        assertArrayEquals(expectedLinks.toArray(), links.toArray());
     }
 }
---
+++ MarkdownParseTestOld.java
@@ -0,0 +1,105 @@
+
+// Imports tools from junit
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+// Class definition
+public class MarkdownParseTest {
+    // Defines following function as a Test
+    @Test
+    // Method definition
+    public void addition() {
+        // Checks that 1+1 = 2
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void thisTestNoLongerFails() {
+        assertEquals(-1.0 / 12, 11.0 / 12 - 1.0, 0.1);
+    }
+
+    private ArrayList<String> getLinksTester(String fileName) throws IOException {
+        Path file = Path.of(fileName);
+        String content = Files.readString(file);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        return links;
+    }
+
+    @Test
+    public void testFileTest() throws IOException {
+        ArrayList<String> links = getLinksTester("test-file.md");
+        List<String> expectedLinks = List.of(
+                "https://something.com",
+                "some-thing.html");
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void breakingFileTest() throws IOException {
+        ArrayList<String> links = getLinksTester("breaking-file.md");
+        List<String> expectedLinks = List.of(
+                "https://something.com",
+                "some-thing.html");
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void imageBugTester() throws IOException {
+        ArrayList<String> links = getLinksTester("image-bug.md");
+        List<String> expectedLinks = List.of(
+                "https://something.com",
+                "some-thing.html");
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void spacedLinkTester() throws IOException {
+        ArrayList<String> links = getLinksTester("spaced-link.md");
+        List<String> expectedLinks = List.of();
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void noLinksTester() throws IOException {
+        ArrayList<String> links = getLinksTester("no-links.md");
+        List<String> expectedLinks = List.of();
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void spacedLinkTester2() throws IOException {
+        ArrayList<String> links = getLinksTester("spaced-links-with-other-links.md");
+        List<String> expectedLinks = List.of(
+                "google.com");
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void newFailedTest() throws IOException {
+        ArrayList<String> links = getLinksTester("nestedParenthesisFailure.md");
+        List<String> expectedLinks = List.of();
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void group2Break() throws IOException {
+        ArrayList<String> links = getLinksTester("group2-break.md");
+        List<String> expectedLinks = List.of();
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+
+    @Test
+    public void test3() throws IOException {
+        ArrayList<String> links = getLinksTester("one-space-file.md");
+        List<String> expectedLinks = List.of();
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
+}
